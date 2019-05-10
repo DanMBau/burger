502ad8923684741da3e20095378d5f998d1796d9
DocHolidayDeluxe <dabauer@me.com>
2019-05-10 19:16:49 -0400

    file structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('The Big Kahuna', false);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Triple', false);
+INSERT INTO burgers (name, devoured) VALUES ('Sidecar', false);
