ece1986de30fc45a30827942a81b345f53aa0ec4
DocHolidayDeluxe <dabauer@me.com>
2019-05-13 19:04:08 -0400

    heroku3

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}
+else{
+connection = mysql.createConnection({
+  
+host: "localhost",
   port: 3306,
   user: "root",
   password: "",
@@ -17,6 +24,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
+}
 // Export connection for our ORM to use.
 module.exports = connection;
