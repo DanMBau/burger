04fa08c74869daf2b227e2851476aa186dca6020
DocHolidayDeluxe <dabauer@me.com>
2019-05-14 17:51:00 -0400

    all

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
-INSERT INTO burgers (name, devoured) VALUES ('The Big Kahuna', false);
-INSERT INTO burgers (name, devoured) VALUES ('Veggie Triple', false);
-INSERT INTO burgers (name, devoured) VALUES ('Sidecar', false);
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Big Kahuna", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Triple", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sidecar", false);
