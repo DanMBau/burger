8a1b667c4b02a94467dd82e2211d78a81a519b71
DocHolidayDeluxe <dabauer@me.com>
2019-05-14 17:48:41 -0400

    all

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@
 var mysql = require("mysql");
 
 var connection;
+
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 
@@ -12,7 +13,7 @@ connection = mysql.createConnection({
 host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "!subway12A",
   database: "burgers_db"
 });
 
